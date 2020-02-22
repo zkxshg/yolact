897490ea1e3be3d7b45d04c48f0055ec2aefec03
zkxshg <35519607+zkxshg@users.noreply.github.com>
2020-02-22 14:23:15 +0800

    Add files via upload

---
+++ efficientnet/__init__.py
@@ -0,0 +1,10 @@
+__version__ = "0.5.1"
+from .efficinetnet import EfficientNet
+from .utils import (
+    GlobalParams,
+    BlockArgs,
+    BlockDecoder,
+    efficientnet,
+    get_model_params,
+)
+
