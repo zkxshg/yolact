13017e561b6a49cef8b7745a1561242b68138fcb
zkxshg <35519607+zkxshg@users.noreply.github.com>
2020-05-06 11:05:26 +0800

    Update detection.py

--- layers/functions/detection.py
+++ layers/functions/detection.py
@@ -226,3 +226,53 @@ class Detect(object):
 
         # Undo the multiplication above
         return boxes[idx] / cfg.max_size, masks[idx], classes, scores
+    
+    def cpu_soft_nms(self, boxes, masks, scores, iou_threshold=0.5, conf_thresh=0.05):
+        # 读入 .pyx
+        import pyximport
+        pyximport.install(setup_args={"include_dirs":np.get_include()}, reload_support=True)
+        # from utils.cython_nms import nms as cnms
+        from utils.cython_nms import softnms_cpu_torch as snms
+        # 初始化
+        num_classes = scores.size(0)
+        idx_lst = []
+        cls_lst = []
+        scr_lst = []
+        
+        # Multiplying by max_size is necessary because of how cnms computes its area and intersections
+        boxes = boxes * cfg.max_size
+
+        for _cls in range(num_classes):
+            cls_scores = scores[_cls, :]
+            conf_mask = cls_scores > conf_thresh
+            idx = torch.arange(cls_scores.size(0), device=boxes.device)
+
+            cls_scores = cls_scores[conf_mask]
+            idx = idx[conf_mask]
+
+            if cls_scores.size(0) == 0:
+                continue
+            
+            preds = torch.cat([boxes[conf_mask], cls_scores[:, None]], dim=1).cpu().numpy()
+            # 应用nms
+            # keep = cnms(preds, iou_threshold)
+            keep = snms(preds)
+            keep = torch.Tensor(keep, device=boxes.device).long()
+
+            idx_lst.append(idx[keep])
+            cls_lst.append(keep * 0 + _cls)
+            scr_lst.append(cls_scores[keep])
+        
+        idx     = torch.cat(idx_lst, dim=0)
+        classes = torch.cat(cls_lst, dim=0)
+        scores  = torch.cat(scr_lst, dim=0)
+
+        scores, idx2 = scores.sort(0, descending=True)
+        idx2 = idx2[:cfg.max_num_detections]
+        scores = scores[:cfg.max_num_detections]
+
+        idx = idx[idx2]
+        classes = classes[idx2]
+
+        # Undo the multiplication above
+        return boxes[idx] / cfg.max_size, masks[idx], classes, scores
